b26f9b41c0fbeb58f018e3d4b97b4906768ec74f
vikas-gandham <gandhamvikas@gmail.com>
2025-05-02 12:57:48 +0000

    fix: fixed padding for card grid

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -3,8 +3,8 @@ import Card from '@/components/Card/Card';
 
 const CardContainer = ({ data }) => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 pt-15 items-start justify-items-center gap-2'>
-      {data.map((media: {fields: {title:string, description1:string, image: string}, sys:{id:string}}) => {
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 py-10  items-start justify-items-center gap-x-4 gap-y-6'>
+      {data.map((media: { fields: { title: string; description1: string; image: string }; sys: { id: string } }) => {
         const { fields } = media;
         const { title, description1, image } = fields;
         return <Card key={media.sys.id} title={title} description={description1} image={image} />;
